6808e97eb9891fef75ce6ad9c96893065c2a91c7
Abdo Atef <abdoatef861@gmail.com>
2023-08-24 12:35:29 +0200

    changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 
 import {startApp} from './home.js'
 
-new startApp();
-
+$(document).ready(function(){
+  $('.spinner').fadeOut(1000 , function(){
+    $('.loading').fadeOut(1000);
+    $('body').css('overflow','auto')
+  });
+  new startApp();
+})
+  
